d1a009c26375d3182ec8f020b8289651fa78fa8b
fakheroo <smidafakher20@gmail.com>
2024-10-30 15:07:37 +0100

    first commit

---
+++ color.js
@@ -0,0 +1,14 @@
+const colorbox=document.getElementById('color-box');
+const box=document.getElementById('change-color-btn');
+
+function getRandomColor () {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+box.addEventListener('click',function(){
+    colorbox.style.backgroundColor =getRandomColor();
+});
